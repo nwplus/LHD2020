40335096d924b8d3486f9f6d78c45ab7a231dd90
Luce <lucy_liu@live.ca>
2019-09-02 22:24:53 -0700

    add Buttons to each event

--- components/Events.vue
+++ components/Events.vue
@@ -26,17 +26,8 @@
           <p class="text">
             {{ item.text }}
           </p>
-          <p v-if="item.title === 'cmd-f'" class="disclaimer">
-            *The cmd-f team would like to acknowledge that "female" or "women" is not an
-            accurate description for many people and it may make some feel unwelcome.
-            We use * to specifically and intentionally include cis and trans women,
-            as well as non-binary, agender, intersex people.
-          </p>
-          <a
-            :href="item.learnMoreLink"
-            target="_blank"
-            class="button is-primary is-outlined learnMoreButton"
-          >Learn More</a>
+          <Button title="Email list" url="#" />
+          <Button title="Event page" url="#" />
         </div>
       </div>
     </div>
@@ -44,8 +35,12 @@
 </template>
 
 <script>
+import Button from '~/components/Button.vue'
 import orderBy from 'lodash.orderby'
 export default {
+  components: {
+    Button
+  },
   props: {
     items: {
       type: Array,

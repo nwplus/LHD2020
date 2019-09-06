31f6a0ce61d75dc96327a7f01045fd85bebb5703
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-09-06 22:35:54 +0000

    Deploy nwplus/LHD2020 to github.com/nwplus/LHD2020.git:gh-pages

--- models/FAQ.ts
+++
@@ -1,8 +0,0 @@
-interface FAQ {
-    //Question for the FAQ
-    question: string,
-    //Answer to the question
-    answer: string,
-    //Catagory for this faq question (currently unused)
-    catagory: string
-}
--- models/Sponsors.ts
+++
@@ -1,8 +0,0 @@
-interface Sponsors {
-    //Alt name for the sponsor
-    name: String,
-    //URL for the image to link to
-    url: URL,
-    //Image name in our storage bucket
-    image: String
-}

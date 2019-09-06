53d12cd399d458c2186e526e1e50e9332dad2bf3
Giulio Rossi <ciuffi9@mac.com>
2019-09-06 16:40:16 -0700

    fix button conflict

--- components/Button.vue
+++ components/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="`wrapper ${disabled ? 'disabled' : ''}`">
+  <div class="wrapper">
     <a :href="url" :class="`but-wrapped ${!disabled ? 'hoverable' : ''}`">
       <p :class="`but-text ${disabled ? 'disabled' : ''}`"> {{ title }}</p>
       <img src="~@/assets/button.png" :class="`but ${disabled ? 'disabled' : ''} ${isWide ? 'wide' : ''}`">
@@ -32,64 +32,36 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import "bulma/bulma.sass";
-
-.wrapper {
-  text-align: center;
-  display: inline-block;
-  margin: 0 5px 0 5px;
-  @include until($desktop) {
-    margin: 0;
-  }
-}
-.but-wrapped {
-  display: inline-block;
-  transition: all 0.2s ease-in-out;
-  margin: auto;
-  width: 142px;
-  height: 53px;
-  @include until($desktop) {
-    width: 100px;
-    height: auto;
-  }
-}
-.hoverable:hover {
-  transform: scale(1.1);
-}
-.but-text {
-  position: relative;
-  top: 15px;
-  z-index: 3;
-  color: $dark-blue;
-  font-family: $body-font;
-  font-size: 20px;
-  line-height: 25px;
-  font-weight: bold;
-  @include until($desktop) {
-    top: 9px;
-    font-size: 14px;
-    line-height: 18px;
+  .wrapper {
+    text-align: center;
     display: inline-block;
+    margin: 0 5px 0 5px;
   }
-}
-.disabled {
-  pointer-events:none;
-  cursor: default;
-  color: grey;
-}
-.but {
-  position: relative;
-  margin-top: -20px;
-  z-index: 0;
-  max-width: 121px;
-  height: auto;
-  @include until($desktop) {
-    max-width: 100px;
-    height: auto;
+  .but-wrapped {
+    display: inline-block;
+    transition: all .2s ease-in-out;
+    margin: auto;
+    width: 142px;
+    height: 53px;
+    color: #425E96;
+  }
   .hoverable:hover {
     transform: scale(1.1);
   }
   .wide {
     transform: scaleX(1.2);
+  }
+  .but-text {
+    position: relative;
+    top: 20px;
+    z-index: 3;
+  }
+  .disabled {
+    color: grey;
+  }
+  .but {
+    position: relative;
+    margin-top: -20px;
+    z-index: 0;
   }
 </style>
